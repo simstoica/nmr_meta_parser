4f3cf78cfc4757f6c3e0fd2e8f83f9a42ad75421
Simona <s.stoica@rug.nl>
2022-11-05 22:19:29 +0100

    add logging v1.0

--- nmr_meta_binder.py
+++ nmr_meta_binder.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import logging
 
 import irods.path
 import irods.session
@@ -21,29 +22,38 @@ class NMR_meta_binder():
     def execute(self):
         associated_groups = get_subdirectories(self.path_to_local_folder)
         for associated_group in associated_groups:
+            logging.info(f'Scanning `{associated_group}')
             personal_folders = get_subdirectories(associated_group)
 
             for folder in personal_folders:
+                logging.info(f'Scanning `{folder}')
                 experiments_list = get_subdirectories(folder)
                 for experiment in experiments_list:
                     self.bind_metadata(experiment)
 
     def bind_metadata(self, experiment_folder):
+        logging.info(f'Analyzing `{experiment_folder}`')
         experiment_folder_on_irods = self._get_path_on_irods_server(experiment_folder)
+        logging.info(f'\t\t Corresponding irods folder: `{experiment_folder_on_irods}`')
+
         if not self.irods_connector.session.collections.exists(experiment_folder_on_irods):
+            logging.warning(f'\t\t Irods collection does not exist `{experiment_folder_on_irods}`')
             return
 
         irods_collection_of_experiment = self.irods_connector.session.collections.get(experiment_folder_on_irods)
-
         if irods_collection_of_experiment.metadata.get_all(SYSTEM_NMR_META):
+            logging.warning(f'\t\tIrods collection `{experiment_folder_on_irods}` already has nmr metadata')
             return
 
         metadata = nmr_parser.parse(experiment_folder)
         if not metadata:
+            logging.warning(f'\t\t Cannot extract metadata for experiment `{experiment_folder}`')
             return
 
         prepared_metadata = self._prepare_metadata(metadata)
-        self.irods_connector.addMultipleMetadata([irods_collection_of_experiment], prepared_metadata)
+        logging.debug(f'\t\tPrepared metadata {prepared_metadata}')
+        if not self.irods_connector.addMultipleMetadata([irods_collection_of_experiment], prepared_metadata):
+            logging.error(f'Could not attach metadata to `{experiment_folder_on_irods}`')
 
     def _prepare_metadata(self, metadata):
         prepared_meta = [
@@ -54,6 +64,8 @@ class NMR_meta_binder():
         for (key, value, unit) in metadata:
             if key == 'Parameter_file':
                 value = self._get_path_on_irods_server(value)
+            if not value:
+                value = 'NA'
             prepared_meta.append([f'{key}', f'{value}', f'{unit}'])
         return prepared_meta
 
